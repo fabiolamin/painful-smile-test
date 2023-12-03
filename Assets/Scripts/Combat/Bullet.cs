9a129dadb8255bab13f565427cf488ff6c3f9f72
fabiolamin <fabio_lamin@hotmail.com>
2023-12-03 09:24:51 -0300

    Develop a script for the ship bullet

---
+++ Assets/Scripts/Combat/Bullet.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using PirateGame.Health;
+
+namespace PirateGame.Combat
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        public float Damage { get; set; }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag != gameObject.tag)
+            {
+                HitTarget(collision.gameObject);
+                gameObject.SetActive(false);
+            }
+        }
+
+        private void HitTarget(GameObject target)
+        {
+            ShipHealth shipHealth = target.GetComponent<ShipHealth>();
+
+            if (shipHealth != null)
+            {
+                shipHealth.GetDamage(Damage, gameObject);
+            }
+        }
+
+        public void MoveTowardsTo(Vector2 direction)
+        {
+            _rigidbody.AddForce(direction);
+        }
+    }
+}
+
