d61e46e51221de28daa1c99272939c87fc1fd9b2
fabiolamin <fabio_lamin@hotmail.com>
2023-12-04 18:28:11 -0300

    Implements Enemy Shooter AI

---
+++ Assets/Scripts/AI/Enemy/EnemyShooterAI.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PirateGame.AI.Enemy
+{
+    public class EnemyShooterAI : EnemyAI
+    {
+        [SerializeField] private UnityEvent<Vector2> _onShootingTriggered;
+
+        protected new void Update()
+        {
+            base.Update();
+
+            if (IsTargetNear())
+            {
+                isReadyToAttackPlayer = true;
+            }
+        }
+        private bool IsTargetNear()
+        {
+            return Vector2.Distance(transform.position, target.transform.position) <= shipCombatData.MinShootingDistance;
+        }
+
+        protected override void SetDestination()
+        {
+            navigationAgent.Destination = navigationAgent.PathFinder.WaypointData.GetRandomWaypoint(transform.position).transform;
+        }
+
+        protected override void AttackTarget()
+        {
+            Vector2 targetDirection = target.transform.position - transform.position;
+            _onShootingTriggered.Invoke(targetDirection);
+        }
+    }
+}
+
