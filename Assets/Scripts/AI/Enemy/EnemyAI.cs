4534490770fe25f87047ba25cb7e8bcb6e3d6c77
fabiolamin <fabio_lamin@hotmail.com>
2023-12-04 18:27:23 -0300

    Develop Enemy AI script

---
+++ Assets/Scripts/AI/Enemy/EnemyAI.cs
@@ -0,0 +1,59 @@
+using PirateGame.AI.Navigation;
+using PirateGame.Data.Combat;
+using PirateGame.Health;
+using UnityEngine;
+
+namespace PirateGame.AI.Enemy
+{
+    public abstract class EnemyAI : MonoBehaviour
+    {
+        private float _timeSinceLastAttack = 0f;
+
+        [SerializeField] protected ShipCombatData shipCombatData;
+        [SerializeField] protected NavigationAgent navigationAgent;
+
+        protected Target target;
+        protected ShipHealth targetHealth;
+        protected bool isReadyToAttackPlayer = false;
+
+        public Vector2 SpawnOrigin { get; private set; }
+
+        public NavigationAgent NavigationAgent { get { return navigationAgent; } }
+
+        private void Awake()
+        {
+            target = FindObjectOfType<Target>();
+            targetHealth = target.GetComponent<ShipHealth>();
+        }
+
+        protected virtual void Update()
+        {
+            navigationAgent.Move();
+            CheckAttack();
+        }
+
+        protected virtual void OnEnable()
+        {
+            SetDestination();
+        }
+
+        private void CheckAttack()
+        {
+            if (isReadyToAttackPlayer)
+            {
+                _timeSinceLastAttack += Time.deltaTime;
+
+                if (_timeSinceLastAttack >= shipCombatData.ReloadingTime)
+                {
+                    AttackTarget();
+                    isReadyToAttackPlayer = false;
+                    _timeSinceLastAttack = 0f;
+                }
+            }
+        }
+
+        protected abstract void SetDestination();
+        protected abstract void AttackTarget();
+    }
+}
+
---
+++ Assets/Scripts/AI/Enemy/Target.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace PirateGame.AI.Enemy
+{
+    public class Target : MonoBehaviour
+    {
+
+    }
+}
+
