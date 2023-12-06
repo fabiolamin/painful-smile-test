8028c118cc3923dcbac16943729ea5a43c34ca56
fabiolamin <fabio_lamin@hotmail.com>
2023-12-06 19:43:25 -0300

    Organize pooled objects by setting a parent for each of them

--- Assets/Scripts/Utils/ObjectPooling.cs
+++ Assets/Scripts/Utils/ObjectPooling.cs
@@ -6,6 +6,10 @@ namespace PirateGame.Utils
 {
     public class ObjectPooling : MonoBehaviour
     {
+        private const string _pooledObjectsTagName = "Pooled Objects";
+
+        private Transform _pooledObjectTransform;
+
         [SerializeField] private GameObject _objectPrefab;
         [SerializeField] private int _amount = 10;
 
@@ -13,17 +17,27 @@ namespace PirateGame.Utils
 
         private void Awake()
         {
-            InstantiateObjects();
+            _pooledObjectTransform = GameObject.FindGameObjectWithTag(_pooledObjectsTagName).transform;
+
+            StartInstantiatingObjects();
         }
 
-        private void InstantiateObjects()
+        private void StartInstantiatingObjects()
         {
             for (int i = 0; i < _amount; i++)
             {
-                GameObject pooledObject = Instantiate(_objectPrefab);
-                pooledObject.SetActive(false);
-                PooledObjects.Add(pooledObject);
+                InstantiateObject();
             }
+        }
+
+        private GameObject InstantiateObject()
+        {
+            GameObject pooledObject = Instantiate(_objectPrefab);
+            pooledObject.SetActive(false);
+            PooledObjects.Add(pooledObject);
+            pooledObject.transform.SetParent(_pooledObjectTransform);
+
+            return pooledObject;
         }
 
         public GameObject GetObject()
@@ -31,8 +45,7 @@ namespace PirateGame.Utils
             GameObject pooledObject = PooledObjects.FirstOrDefault(o => !o.activeSelf);
             if (pooledObject == null)
             {
-                pooledObject = Instantiate(_objectPrefab);
-                PooledObjects.Add(pooledObject);
+                pooledObject = InstantiateObject();
             }
 
             pooledObject.SetActive(true);
